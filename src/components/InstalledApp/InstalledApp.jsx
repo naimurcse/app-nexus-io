5e2b13c36bc48b363ce6ac465d5e0f92afbf0568
Naimur Rahaman <2015@gmail.com>
2026-03-23 11:30:15 +0600

    Display all installed data in Installation.jsx; InstalledApp.jsx is Created and Completed UI

---
+++ src/components/InstalledApp/InstalledApp.jsx
@@ -0,0 +1,56 @@
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { formatCount } from "./../../utilities/utilities";
+
+const InstalledApp = ({ app }) => {
+  console.log(app);
+  const { image, title, downloads, ratingAvg } = app;
+  return (
+    <>
+      <div className="flex justify-between items-center bg-white py-3 px-4 gap-3 shadow-md">
+        <div>
+          <div className="flex gap-3">
+            {/* Image */}
+            <div className="w-40 md:w-56 lg:w-20 shrink-0 aspect-square overflow-hidden rounded-md">
+              <img className="w-full h-full object-cover" src={image} alt="" />
+            </div>
+
+            {/* Info */}
+            <div className="py-2 flex flex-col justify-between">
+              <h3 className="text-[20px] font-semibold"> {title}</h3>
+              <div className="flex gap-5 text-[16px] font-normal">
+                <div className="flex items-center gap-1 text-green-500 font-normal">
+                  {/* <FaDownload /> */}
+                  <LuDownload />
+                  {formatCount(downloads)}
+                </div>
+                <div className="flex items-center gap-1 text-orange-400 font-normal">
+                  {/* <FaStar /> */}
+                  {ratingAvg == 5 || ratingAvg >= 4 ? (
+                    <FaStar />
+                  ) : ratingAvg >= 2 ? (
+                    <FaStarHalfAlt />
+                  ) : (
+                    <FaRegStar />
+                  )}
+                  {ratingAvg}
+                </div>
+
+                <p className="text-[#627382]">258M</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Button */}
+        <div>
+          <button className="btn btn-success text-white font-normal">
+            Uninstall
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InstalledApp;
--- src/pages/Installation/Installation.jsx
+++ src/pages/Installation/Installation.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useLoaderData } from "react-router";
 import { getFromStoreDB } from "../../utilities/addToDB";
+import InstalledApp from "./../../components/InstalledApp/InstalledApp";
 
 const Installation = () => {
   const allApps = useLoaderData();
   console.log(allApps);
-  const [installApps, setInstallApps] = useState();
+  const [installApps, setInstallApps] = useState([]);
 
   useEffect(() => {
     const appFromDB = getFromStoreDB();
@@ -20,8 +21,54 @@ const Installation = () => {
   console.log(installApps);
 
   return (
-    <div className="max-w-7xl mx-auto py-20">
-      <h2>I'm Installation</h2>
+    <div className="max-w-7xl mx-auto my-[60px]">
+      {/* Headings */}
+      <div className="text-center">
+        <h2 className="text-[48px] font-bold mb-1">Your Installed Apps</h2>
+        <p className="text-[#627382] text-lg mb-14">
+          Explore All Trending Apps on the Market developed by us
+        </p>
+      </div>
+
+      {/* Sorts */}
+      <div className="flex items-center justify-between mb-6">
+        <div className="font-bold text-xl">
+          ({installApps.length}) Apps Found
+          {/* (44) Apps Found */}
+        </div>
+        <div>
+          <label className="input bg-gray-100 w-100">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search" />
+          </label>
+        </div>
+      </div>
+
+      {/* Installed App */}
+      <div className="grid grid-cols-1 gap-4">
+        {
+          <Suspense fallback={<span>Loading....</span>}>
+            {installApps.map((app) => (
+              <InstalledApp key={app.id} app={app}></InstalledApp>
+            ))}
+          </Suspense>
+        }
+      </div>
     </div>
   );
 };

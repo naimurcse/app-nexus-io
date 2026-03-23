7045bdd0183c5c7e57ba83d54c22a470b2c3c189
Naimur Rahaman <2015@gmail.com>
2026-03-23 11:47:05 +0600

    Added hover effect in Footer; Updated UI in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,10 +8,12 @@ const Footer = () => {
       <footer className="bg-[#001931]">
         <div className="max-w-7xl mx-auto grid grid-cols-12 gap-20 text-neutral-content pt-20 pb-10 border-b-1 border-b-cyan-950">
           <div className="col-span-6">
-            <h2 className="text-4xl font-light leading-10 mb-8">
+            <h2 className="text-4xl font-light leading-12 mb-10">
               Discover Every App You Need, All in One Place.
             </h2>
-            <Link to="/" className="flex justify-start items-center pb-16">
+
+            {/* Logo */}
+            <Link to="/" className="flex justify-start items-center pb-7">
               <img
                 className="w-[50px]"
                 src={Logo}
@@ -24,6 +26,7 @@ const Footer = () => {
               <h1 className="text-5xl font-extrabold ml-5">AppNexus.IO</h1>
             </Link>
           </div>
+
           <div className="col-span-3 flex flex-col justify-between">
             <div>
               <h3 className="footer-title">Location</h3>
@@ -31,23 +34,23 @@ const Footer = () => {
             </div>
             <div>
               <h3 className="footer-title">Social Media</h3>
-              <ul className="flex gap-5">
-                <li className="border-1 border-[#975EEE] p-3 rounded-4xl text-lg">
+              <ul className="flex gap-4">
+                <li className="border-1 border-[#632ee3] transition-all duration-400 hover:bg-[#632ee3] p-3 rounded-4xl text-lg">
                   <a href="#">
                     <FaFacebookF />
                   </a>
                 </li>
-                <li className="border-1 border-[#975EEE] p-3 rounded-4xl text-lg">
+                <li className="border-1 border-[#632ee3]  transition-all duration-400  hover:bg-[#632ee3] p-3 rounded-4xl text-lg">
                   <a href="#">
                     <FaXTwitter />
                   </a>
                 </li>
-                <li className="border-1 border-[#975EEE] p-3 rounded-4xl text-lg">
+                <li className="border-1 border-[#632ee3]  transition-all duration-400  hover:bg-[#632ee3] p-3 rounded-4xl text-lg">
                   <a href="#">
                     <FaInstagram />
                   </a>
                 </li>
-                <li className="border-1 border-[#975EEE] p-3 rounded-4xl text-lg">
+                <li className="border-1 border-[#632ee3]  transition-all duration-400  hover:bg-[#632ee3] p-3 rounded-4xl text-lg">
                   <a href="#">
                     <FaLinkedinIn />
                   </a>
@@ -86,7 +89,7 @@ const Footer = () => {
         </div>
 
         {/* Lower Part */}
-        <div className="text-white font-light opacity-50 max-w-7xl mx-auto flex justify-between pt-5 pb-20">
+        <div className="text-white font-light opacity-70 max-w-7xl mx-auto flex justify-between pt-4 pb-20">
           <p>© 2026 AppNexus.IO – Apps. All rights reserved.</p>
           <div className="flex gap-6">
             <a href="#">License</a>
